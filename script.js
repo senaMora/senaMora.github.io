4fde6f1441e26a655f73902970cd4e5ebf1f54ed
senaMora <senarathnalpsuk.21@uom.lk>
2024-07-16 19:18:27 +0530

    make cal

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function add() {
   let n1 = document.getElementById("num1").value;
   let n2 = document.getElementById("num2").value;
 
-  let result = n1 + n2;
-  document.getElementById("output").innerHTML = result;
+  let result = parseFloat(n1) + parseFloat(n2);
+  document.getElementById("result").innerHTML = "result is: " + result;
 }

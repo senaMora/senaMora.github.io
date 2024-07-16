5b1337ec95c972a023bb6b2a18f01debb5a0987a
Kavindu Senarathna <129992861+senaMora@users.noreply.github.com>
2024-07-16 18:06:33 +0530

    Update script.js

--- doc/script.js
+++ doc/script.js
@@ -1,6 +1,6 @@
 function add() {
-  let n1 = getElementById("num1").value;
-  let n2 = getElementById("num2").value;
+  let n1 = document.getElementById("num1").value;
+  let n2 = document.getElementById("num2").value;
 
   let result = n1 + n2;
   getElementById("output").innerHTML = result
